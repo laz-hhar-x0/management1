4744c59be4bb83defce3e7ec06f85352c71c546d
principia <principia480@gmail.com>
2025-04-25 21:31:34 +0100

    Upload-5

--- public/js/main.js
+++ public/js/main.js
@@ -18,7 +18,7 @@ const toggleSidebar = () => {
 
 let targetUrl = ""; 
 let targetForm = null;
-let currentAction = ""; // 🆕 لتحديد نوع العملية
+let currentAction = ""; 
 
 function promptCode(url, action) {
   targetUrl = url;
@@ -40,50 +40,17 @@ function closePrompt() {
   document.getElementById("codeInput").value = "";
   targetForm = null;
 }
-// function checkCode() {
-//   const correctCode = "admin@"; 
-//   const enteredCode = document.getElementById("codeInput").value;
-//   if (enteredCode === correctCode) {
-//     window.location.href = targetUrl;
-//   } else {
-//     alert("🚫 Incorrect code .. ! try Again");
-//   }
-// }
 
 
 
 
-
-//   function showCodePrompt(action, url, form = null) {
-//     const promptDiv = document.getElementById("codePrompt");
-//     promptDiv.style.display = "block";
-
-//     const submitBtn = promptDiv.querySelector("button.btn-primary");
-//     const input = document.getElementById("codeInput");
-
-//     // Reset previous value
-//     input.value = "";
-
-//     submitBtn.onclick = function () {
-//       const code = input.value;
-//       if (code === "admin@") {
-//         if (action === "edit") {
-//           window.location.href = url;
-//         } else if (action === "delete" && form) {
-//           form.submit();
-//         }
-//       } else {
-//         alert("Incorrect code !!");
-//       }
-//     };
-//   }
 
  
 
 function showCodePrompt(action, url, form = null) {
-  currentAction = action; // 🆕 حفظ نوع العملية (edit أو delete)
-  targetUrl = url;        // 🆕 حفظ الرابط
-  targetForm = form;      // 🆕 حفظ الفورم إن وُجد
+  currentAction = action; 
+  targetUrl = url;        
+  targetForm = form;     
 
   document.getElementById("codePrompt").style.display = "block";
   document.getElementById("codeInput").value = "";
@@ -96,31 +63,6 @@ function showCodePrompt(action, url, form = null) {
 }
 
 
-
-// corect 
-
-// function checkCode() {
-//   const enteredCode = document.getElementById("codeInput").value;
-
-//   if (currentAction === "edit" && enteredCode === "admin@@") {
-//     window.location.href = targetUrl;
-//   } 
-//   else if (currentAction === "delete" && enteredCode === "admin@") {
-//     targetForm.submit();
-//   } 
-//   else if (currentAction === "addProf" && enteredCode === "admin@prof") {
-//     window.location.href = targetUrl;
-//   } 
-//   else if (currentAction === "addEtud" && enteredCode === "admin@etud") {
-//     window.location.href = targetUrl;
-//   } 
-//   else {
-//     alert("🚫 Incorrect code for this action!");
-//     return;
-//   }
-
-//   closePrompt(); // بعد العملية
-// }
 
 
 function checkCode() {
@@ -143,5 +85,5 @@ function checkCode() {
     return;
   }
 
-  closePrompt(); // بعد العملية
+  closePrompt(); 
 }
